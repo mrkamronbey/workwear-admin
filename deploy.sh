0d6bcdb9a815c187a0924e85b5cebdc70d28d539
uzxakker <103135051+uzxakker@users.noreply.github.com>
2023-08-08 12:14:19 +0500

    push go

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* root@62.217.179.24:/var/www/top-admin/
+scp -r build/* root@62.217.179.24:/var/www/odejda-admin/
 
 echo "Done!"
